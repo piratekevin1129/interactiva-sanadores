6ce4f9c058abb2e1316ce50bb2f73a7768695f6c
Kevin Gaviria Fonnegra <desarrollo3@virtualcolors.com>
2025-10-22 03:04:35 -0500

    version 0.1

---
+++ assets/data.js
@@ -0,0 +1,25 @@
+var cuadros = [
+    {
+        items:[{
+            foto:'1.jpg',
+            audio:null,
+            descripcion:"Si nos aplica la protección de especies, se debe tener un plan de acción (hecho por profesional ambiental u asociación). Ejemplos de acciones: trasplante de especies, refugio temporal en obra, prohibición de consumo de especies nativas en peligro de extinción, etc"
+        },{
+            foto:'1.jpg',
+            audio:null,
+            descripcion:"Si nos aplica la protección de especies, se debe tener un plan de acción (hecho por profesional ambiental u asociación). Ejemplos de acciones: trasplante de especies, refugio temporal en obra, prohibición de consumo de especies nativas en peligro de extinción, etc"
+        },{
+            foto:'1.jpg',
+            audio:null,
+            descripcion:"Si nos aplica la protección de especies, se debe tener un plan de acción (hecho por profesional ambiental u asociación). Ejemplos de acciones: trasplante de especies, refugio temporal en obra, prohibición de consumo de especies nativas en peligro de extinción, etc"
+        },{
+            foto:'1.jpg',
+            audio:null,
+            descripcion:"Si nos aplica la protección de especies, se debe tener un plan de acción (hecho por profesional ambiental u asociación). Ejemplos de acciones: trasplante de especies, refugio temporal en obra, prohibición de consumo de especies nativas en peligro de extinción, etc"
+        },{
+            foto:'1.jpg',
+            audio:null,
+            descripcion:"Si nos aplica la protección de especies, se debe tener un plan de acción (hecho por profesional ambiental u asociación). Ejemplos de acciones: trasplante de especies, refugio temporal en obra, prohibición de consumo de especies nativas en peligro de extinción, etc"
+        }]
+    }
+]
