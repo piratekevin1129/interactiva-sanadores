316b1d1f3dbe05ace3fc7e086159f864cb074ad6
SanquihUdeA <santiago.quinteroh@udea.edu.co>
2025-10-22 03:32:57 -0500

    version 0.3

--- assets/data.js
+++ assets/data.js
@@ -1,23 +1,23 @@
 var cuadros = [
     {
         items:[{
-            foto:'1.jpg',
+            foto:'1.png',
             audio:null,
             descripcion:"Si nos aplica la protección de especies, se debe tener un plan de acción (hecho por profesional ambiental u asociación). Ejemplos de acciones: trasplante de especies, refugio temporal en obra, prohibición de consumo de especies nativas en peligro de extinción, etc"
         },{
-            foto:'1.jpg',
+            foto:'1.png',
             audio:null,
             descripcion:"Si nos aplica la protección de especies, se debe tener un plan de acción (hecho por profesional ambiental u asociación). Ejemplos de acciones: trasplante de especies, refugio temporal en obra, prohibición de consumo de especies nativas en peligro de extinción, etc"
         },{
-            foto:'1.jpg',
+            foto:'1.png',
             audio:null,
             descripcion:"Si nos aplica la protección de especies, se debe tener un plan de acción (hecho por profesional ambiental u asociación). Ejemplos de acciones: trasplante de especies, refugio temporal en obra, prohibición de consumo de especies nativas en peligro de extinción, etc"
         },{
-            foto:'1.jpg',
+            foto:'1.png',
             audio:null,
             descripcion:"Si nos aplica la protección de especies, se debe tener un plan de acción (hecho por profesional ambiental u asociación). Ejemplos de acciones: trasplante de especies, refugio temporal en obra, prohibición de consumo de especies nativas en peligro de extinción, etc"
         },{
-            foto:'1.jpg',
+            foto:'1.png',
             audio:null,
             descripcion:"Si nos aplica la protección de especies, se debe tener un plan de acción (hecho por profesional ambiental u asociación). Ejemplos de acciones: trasplante de especies, refugio temporal en obra, prohibición de consumo de especies nativas en peligro de extinción, etc"
         }]
