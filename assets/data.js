50310baea2352e7d27a0455405f0a164c8e6f2a2
Kevin Gaviria Fonnegra <desarrollo3@virtualcolors.com>
2025-10-23 03:08:31 -0500

    pendientes imagenes

--- assets/data.js
+++ assets/data.js
@@ -29,5 +29,245 @@ var cuadros = [
             audio:null,
             descripcion:"Procedimiento de comunicación en caso de incidente ambiental grave difundido."
         }]
+    },{
+        title:'Uso responsable <br> <span>del Agua</span>',
+        loaded:false,
+        intro:null,
+        items:[{
+            id:1,
+            foto:null,
+            audio:null,
+            descripcion:"Contadores de agua instalados para monitorear el consumo de agua (obtenida de redes, de perforación de pozos, bombeada de ríos o lagos y el agua drenada para abatir nivel freático)."
+        },{
+            id:2,
+            foto:null,
+            audio:null,
+            descripcion:"Solo se usan aguas de fuentes legales y se cuentan con los permisos de uso."
+        },{
+            id:3,
+            foto:null,
+            audio:null,
+            descripcion:"Sin fugas de agua (en tuberías, mangueras, tanques), ni llaves abiertas desatendidas."
+        },{
+            id:4,
+            foto:null,
+            audio:null,
+            descripcion:"Sensibilización al consumo responsable de agua (oficinas, campamentos de obra, etc)."
+        },{
+            id:5,
+            foto:null,
+            audio:null,
+            descripcion:"Circuitos cerrados de reuso de agua, en lugar de generar aguas residuales."
+        },{
+            id:6,
+            foto:null,
+            audio:null,
+            descripcion:"Sistema de canalización y aprovechamiento de aguas pluviales en obra. O de escorrentía para evitar su contaminación y arrastres fuera de la obra."
+        },{
+            id:7,
+            foto:null,
+            audio:null,
+            descripcion:"Si el vertido de aguas residuales es requerido, se le da tratamiento para cumplir los requisitos legales (permiso de vertido, sólidos en suspensión, Ph después de lavar equipos de hormigonado, etc)."
+        }]
+    },{
+        title:'Efectos locales sostenibles <br> <span>sobre flora y fauna</span>',
+        loaded:false,
+        intro:null,
+        items:[{
+            id:1,
+            foto:null,
+            audio:null,
+            descripcion:"Cumplir las medidas de protección de flora y fauna del permiso ambiental del centro de trabajo aplicables a nuestra actividad."
+        },{
+            id:2,
+            foto:null,
+            audio:null,
+            descripcion:"Si nos aplica la protección de especies, se debe tener un plan de acción (hecho por profesional ambiental u asociación). Ejemplos de acciones: trasplante de especies, refugio temporal en obra, prohibición de consumo de especies nativas en peligro de extinción, etc"
+        },{
+            id:3,
+            foto:null,
+            audio:null,
+            descripcion:"Capacitar a los trabajadores en la identificación de especies protegidas y en “qué hacer” en caso de encontrarlas."
+        },{
+            id:4,
+            foto:null,
+            audio:null,
+            descripcion:"Proteger los árboles circundantes a la obra que pudieran afectarse por nuestras actividades."
+        },{
+            id:5,
+            foto:null,
+            audio:null,
+            descripcion:"Si es necesaria la tala de árboles al interior de la obra, se cuentan con los permisos ambientales."
+        },{
+            id:6,
+            foto:null,
+            audio:null,
+            descripcion:"Utilización de productos químicos amigables con el medio ambiente, como detergentes o aditivos biodegradables."
+        },{
+            id:7,
+            foto:null,
+            audio:null,
+            descripcion:"Si la obra se encuentra en cercanía de un curso de agua, se aplican las medidas necesarias para proteger el medio acuático."
+        },{
+            id:8,
+            foto:null,
+            audio:null,
+            descripcion:"El perímetro de la obra debe estar protegido para evitar fugas al exterior de fluidos o derrames."
+        }]
+    },{
+        title:'Control de la contaminación del Aire<br> <span>Ruido, Polvo, VOC y NOx</span>',
+        loaded:false,
+        intro:null,
+        items:[{
+            id:1,
+            foto:null,
+            audio:null,
+            descripcion:"Cumplir con las restricciones de ruido (niveles y horarios) y medidas de reducción de polvo, VOC (compuestos orgánicos volátiles) y NOx (óxidos de nitrógeno producto de combustión) del permiso ambiental del centro de trabajo aplicables a nuestra actividad."
+        },{
+            id:2,
+            foto:null,
+            audio:null,
+            descripcion:"Aplicar medidas contra el ruido como: realizar mediciones regulares de ruido perimetral, colocar barreras anti ruido para insonorizar los equipos, dar preferencia al uso de motores eléctricos en herramientas, equipos y vehículos."
+        },{
+            id:3,
+            foto:null,
+            audio:null,
+            descripcion:"Usar aguas recicladas en lugar de aguas potables para el control del polvo con aspersión (cuando otras medidas no sean posibles."
+        },{
+            id:4,
+            foto:null,
+            audio:null,
+            descripcion:"Al usar pinturas, que sean siempre: sin VOC (Compuestos Orgánicos Volátiles)."
+        },{
+            id:5,
+            foto:null,
+            audio:null,
+            descripcion:"Para reducir las emisiones de NOx de vehículos y maquinaria con motores de combustión se debe hacer el mantenimiento oportuno (como cambio de aceite, cambio de filtros de aire, etc), usar combustibles sin azufre o añadirle aditivos como el AdBlue y no superar los límites de las pruebas de gases según la legislación local."
+        }]
+    },{
+        title:'Reciclaje y disposición <br> <span>adecuada de Desechos</span>',
+        loaded:false,
+        intro:null,
+        items:[{
+            id:1,
+            foto:null,
+            audio:null,
+            descripcion:"Implementar acciones con proveedores y en obra para reducir la generación de residuos."
+        },{
+            id:2,
+            foto:null,
+            audio:null,
+            descripcion:"Contar con una zona temporal de acopio de residuos. Y darles disposición según tu tipo."
+        },{
+            id:3,
+            foto:null,
+            audio:null,
+            descripcion:"Sensibilizar al personal para usar los contenedores que clasifican los residuos."
+        },{
+            id:4,
+            foto:null,
+            audio:null,
+            descripcion:"Etiquetar los contenedores claramente."
+        },{
+            id:5,
+            foto:null,
+            audio:null,
+            descripcion:"Si hay residuos contaminados con aceites, debe darse disposición especial junto con otros residuos peligrosos (revisar legislación local sobre clasificación de residuos peligrosos)."
+        },{
+            id:6,
+            foto:null,
+            audio:null,
+            descripcion:"Contar con los permisos de disposición de residuos (incluyendo tierras excavadas) y tener trazabilidad de los servicios de disposición."
+        },{
+            id:7,
+            foto:null,
+            audio:null,
+            descripcion:"Reusar in situ desechos de hormigón (cuando sea posible) y aguas de perforación."
+        },{
+            id:8,
+            foto:null,
+            audio:null,
+            descripcion:"Gestionar con externos (asociaciones o empresas) el reciclaje del máximo posible de residuos no peligrosos (plásticos, cartón, chatarra, etc)."
+        },{
+            id:9,
+            foto:null,
+            audio:null,
+            descripcion:"Cumplir la normativa local de calidad de agua en el punto de vertido."
+        }]
+    },{
+        title:'Planeación y Consumo Eficiente <br> <span>de energía, electricidad y combustible',
+        loaded:false,
+        intro:null,
+        items:[{
+            id:1,
+            foto:null,
+            audio:null,
+            descripcion:"Planear las necesidades energéticas de la obra, dando preferencia a la conexión a la red eléctrica (siempre que sea posible). Y un plan de movilidad para hacer más eficientes los traslados."
+        },{
+            id:2,
+            foto:null,
+            audio:null,
+            descripcion:"Si se requieren grupos electrógenos, éstos deben ser del mínimo tamaño. La optimización del tamaño debe estar evidenciada con un cálculo que debe considerar bombas que no excedan la capacidad necesaria (mínimo capacidad para satisfacer requerimientos)."
+        },{
+            id:3,
+            foto:null,
+            audio:null,
+            descripcion:"Todo equipo que NO está trabajando, deben estar apagado (en lugar de estar al Ralenti)."
+        },{
+            id:4,
+            foto:null,
+            audio:null,
+            descripcion:"Instalar contadores de electricidad en las obras para monitorear su consumo."
+        },{
+            id:5,
+            foto:null,
+            audio:null,
+            descripcion:"Sensibilizar al personal en austeridad energética:  apagar luminarias, computadoras, impresoras y aires acondicionados cuando sea innecesario (fuera de horario laboral y fines de semana), regular la temperatura de la calefacción y aires acondicionados."
+        },{
+            id:6,
+            foto:null,
+            audio:null,
+            descripcion:"Instalar o ajustar el sistema de climatización de las maquinarias para independizarlo cuando sea posible del motor principal."
+        },{
+            id:7,
+            foto:null,
+            audio:null,
+            descripcion:"Monitorear los consumos de combustible y electricidad como herramienta para un sistema de recompensas por la reducción de consumos."
+        },{
+            id:8,
+            foto:null,
+            audio:null,
+            descripcion:"Se realiza un seguimiento de las emisiones de CO2 (del scope 1 y 2) de la obra."
+        }]
+    },{
+        title:'Planeación Eficiente de Emisiones y uso de Materiales <br> <span>concreto, cemento y acero</span>',
+        loaded:false,
+        intro:null,
+        items:[{
+            id:1,
+            foto:null,
+            audio:null,
+            descripcion:"Realizar una estimación de huella de carbono de los materiales a usar en la obra."
+        },{
+            id:2,
+            foto:null,
+            audio:null,
+            descripcion:"Realizar seguimiento de las emisiones de CO2 (del scope 3) de la obra."
+        },{
+            id:3,
+            foto:null,
+            audio:null,
+            descripcion:"El acero utilizado en la obra debe contar con al menos un 20% de acero reciclado."
+        },{
+            id:4,
+            foto:null,
+            audio:null,
+            descripcion:"La obra utiliza cementos y/o concretos de bajo carbono."
+        },{
+            id:5,
+            foto:null,
+            audio:null,
+            descripcion:"Todo excedente de acero es enviado a reciclaje."
+        }]
     }
 ]
